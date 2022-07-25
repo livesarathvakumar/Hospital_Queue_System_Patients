a100e9191b22deda2178d6c676149db1fbd550d4
saratheveara <skumar@eveara.com>
2022-07-25 23:32:18 +0530

    patient home page design

--- doctor_patient/lib/screen/patient_home_screen.dart
+++ doctor_patient/lib/screen/patient_home_screen.dart
@@ -35,55 +35,161 @@ class _PatientsHomeScreenState extends State<PatientsHomeScreen> {
     });
   }
 
-  final Query<Map<String, dynamic>> _doctors = FirebaseFirestore.instance
-      .collection('users')
-      .where('role', isEqualTo: 'doctor');
-
   final CollectionReference _products =
       FirebaseFirestore.instance.collection('departments');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Patient Home"),
-        centerTitle: true,
-        actions: [
-          PopupMenuButton<MenuItem>(
-            onSelected: (value) {
-              if (value == MenuItem.profilepage) {
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => UserProfile()));
-              } else if (value == MenuItem.logout) {
-                logout(context);
-              }
-            },
-            itemBuilder: (context) => const [
-              PopupMenuItem(
-                  value: MenuItem.profilepage, child: Text('Profile')),
-              PopupMenuItem(value: MenuItem.logout, child: Text('Logout'))
-            ],
-          )
-        ],
-      ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Container(
-            color: Colors.white,
-            child: Padding(
-              padding: const EdgeInsets.all(36.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[],
-              ),
-            ),
-          ),
+        appBar: AppBar(
+          title: const Text("Patient Home"),
+          centerTitle: true,
+          actions: [
+            PopupMenuButton<MenuItem>(
+              onSelected: (value) {
+                if (value == MenuItem.profilepage) {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => UserProfile()));
+                } else if (value == MenuItem.logout) {
+                  logout(context);
+                }
+              },
+              itemBuilder: (context) => const [
+                PopupMenuItem(
+                    value: MenuItem.profilepage, child: Text('Profile')),
+                PopupMenuItem(value: MenuItem.logout, child: Text('Logout'))
+              ],
+            )
+          ],
         ),
-      ),
-    );
+        body: Column(children: <Widget>[
+          SizedBox(
+            height: 10,
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text('Hi ${loggedInUser.firstName} ',
+                textAlign: TextAlign.right,
+                overflow: TextOverflow.ellipsis,
+                style:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(' Welcome Back',
+                textAlign: TextAlign.right,
+                overflow: TextOverflow.ellipsis,
+                style:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(' Category',
+                textAlign: TextAlign.right,
+                overflow: TextOverflow.ellipsis,
+                style:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          SizedBox(
+            height: 150,
+            child: departmentBox(),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Expanded(
+              child: Container(
+                  margin: EdgeInsets.all(5), child: createDoctorListView()))
+        ]));
   }
 
+  Widget departmentBox() => StreamBuilder(
+      stream: _products.snapshots(),
+      builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+        if (streamSnapshot.hasData) {
+          return ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: streamSnapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              final DocumentSnapshot documentSnapshot =
+                  streamSnapshot.data!.docs[index];
+              return Container(
+                  color: Colors.white,
+                  width: 150,
+                  height: 100,
+                  child: Card(
+                    color: Colors.green,
+                    margin: const EdgeInsets.all(5),
+                    child: ListTile(
+                      onTap: () {
+                        print(documentSnapshot.id);
+                      },
+                      title: Text(documentSnapshot['name'],
+                          textAlign: TextAlign.center,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                              fontSize: 18, color: Colors.white)),
+                    ),
+                  ));
+            },
+          );
+        } else if (streamSnapshot.hasError) {
+          return Text('has error');
+        } else {
+          return Text('no data');
+        }
+      });
+  final Query<Map<String, dynamic>> _doctors = FirebaseFirestore.instance
+      .collection('users')
+      .where('role', isEqualTo: 'doctor')
+      .where('departmentId', isEqualTo: 'QgWKuvxnBvfTIhQqBFgK');
+  Widget createDoctorListView() => StreamBuilder(
+      stream: _doctors.snapshots(),
+      builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+        if (streamSnapshot.hasData) {
+          return ListView.builder(
+            itemCount: streamSnapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              final DocumentSnapshot documentSnapshot =
+                  streamSnapshot.data!.docs[index];
+              return Card(
+                margin: const EdgeInsets.all(10),
+                child: ListTile(
+                  trailing: SizedBox(
+                      width: 100,
+                      height: 100,
+                      child: Row(
+                        children: [
+                          IconButton(
+                              onPressed: () {
+                                final docUser = FirebaseFirestore.instance
+                                    .collection('users')
+                                    .doc(documentSnapshot.id);
+                                docUser.delete();
+                              },
+                              icon: const Icon(Icons.delete))
+                        ],
+                      )),
+                  title: Text(documentSnapshot['firstName']),
+                ),
+              );
+            },
+          );
+        } else if (streamSnapshot.hasError) {
+          return Text('has error');
+        } else {
+          return Text('no data');
+        }
+      });
   // the logout function
   Future<void> logout(BuildContext context) async {
     await FirebaseAuth.instance.signOut();
